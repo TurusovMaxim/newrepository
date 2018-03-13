c6747b6ad87d27f57cf2c7b5994285c364cddcf8
Maxim Turusov <turusov@phys.vsu.ru>
2018-03-13 16:04:46 +0300

    Update Linear search

--- Linear search.py
+++ Linear search.py
@@ -5,7 +5,7 @@ def linear_search(s, x):
     return -1
 
 s = [1, 12, 15, 21, 27, 36, 40, 53, 68, 72]
-x = 37
+x = int(input())
 
 result = linear_search(s, x)
 
@@ -13,3 +13,6 @@ if result == -1:
     print("-1")
 else:
     print("The element is in array at the index", result)
+
+print('Here is the array: 1, 12, 15, 21, 27, 36, 40, 53, 68, 72')
+input('Press "enter" for close program ')
