7c870f6fdac8dfd9afc4b74c0fab53f80a4ef506
Maxim Turusov <turusov@phys.vsu.ru>
2018-03-18 22:02:43 +0300

    Counting of shower water (in 0.5 liter bottles).

---
+++ Water counting.py
@@ -0,0 +1,12 @@
+i = int(input('How many minutes do you plan to take a shower? '))
+
+def counting_water(i):
+    result = []
+    if i > 0:
+        result.append(i * 12)
+        return result
+    else:
+        return print('You can not return the past :)')
+
+print('To do this you will need',counting_water(i),'bottles')
+input('Press "enter" for close program ')
