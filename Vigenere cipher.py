dda9eb716184093165ef0c4a9ae3a6c357fb951c
Maxim Turusov <turusov@phys.vsu.ru>
2018-05-21 23:29:50 +0300

    Vigenere cipher for the Russian alphabet.

---
+++ Vigenere cipher.py
@@ -0,0 +1,30 @@
+element = input('Please enter any word in Russian: ')
+
+def data_checking(element):
+    """Verification of data entered by the user"""
+    while element.isdigit():
+        element = input('Enter any word in Russian: ')
+    else:
+        while True:
+            try:
+                if int(element) < 0:
+                    element = input('Enter any word in Russian: ')
+            except (TypeError, ValueError):
+                return Vigenere_cipher(element)
+
+
+def Vigenere_cipher(element):
+    """The algorithm for this sort"""
+    alphabet = ('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+    key = input('Enter the keyword in Russian too: ')
+    cipher = ''
+    i = 0
+    for c in element:
+        amount = alphabet.find(c) + alphabet.find(key[i % len(key)])
+        cipher_Vigenere = int(amount) % len(alphabet)
+        cipher += alphabet[cipher_Vigenere]
+        i += 1
+    return print(cipher)
+
+print(data_checking(element))
+input('Press any key to end the program ')
